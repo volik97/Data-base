eeaa6a59fe417af11928b43652b1e10c2b7e769e
volik97 <volik97@inbox.ru>
2022-03-31 06:40:05 +0300

    script sql

---
+++ Script.sql
@@ -0,0 +1,24 @@
+create table genre (
+	id serial primary key,
+	genre_name varchar(40) not null
+);
+
+create table author (
+	id serial primary key,
+	nickname varchar(40) not null,
+	id_genre integer references genre(id)
+);
+
+create table album (
+	id serial primary key,
+	album_name text not null,
+	release_year integer not null,
+	id_author integer references author(id)
+);
+
+create table track (
+	id serial primary key,
+	track_name varchar(40) not null,
+	duration real not null,
+	id_album integer references album(id)
+);
